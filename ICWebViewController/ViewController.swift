70614643602eaba3ff38488c517e63c65da52455
陈颜俊 <inter.chen@gmail.com>
2017-03-03 21:10:37 +0800

    Initial Commit

---
+++ ICWebViewController/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ICWebViewController
+//
+//  Created by 陈 颜俊 on 2017/3/3.
+//  Copyright © 2017年 azhunchen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
